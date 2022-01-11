0b1d8e5fc5839a05a9cab0daff3b1765ba7c38d8
Philip Browne <pbrowne@gmail.com>
2022-01-11 11:30:56 -0500

    remove creds

--- backend/creds.js
+++
@@ -1,9 +0,0 @@
-const FROM_EMAIL = 'timely.email.sender@gmail.com';
-const FROM_PW = 'TimelyPW1234';
-
-// These credentials are only being made available because this is a private respository for a programming assessment.
-
-module.exports = {
-  FROM_EMAIL,
-  FROM_PW,
-};
